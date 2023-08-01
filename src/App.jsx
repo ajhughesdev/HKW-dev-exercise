6e1c3b1ffa23bcf7987d870dd41d019be6f39002
Andrew J Hughes <andrew@ajhughes.dev>
2023-08-01 00:31:23 -0400

    Add search bar, column toggler, and table components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from 'react'
+
+import SearchBar from './components/SearchBar'
+import ColumnToggler from './components/ColumnToggler'
+import Table from './components/Table'
+
+import data from './hp-api.json'
 import './App.css'
 
-function App() {
 
+const App = () => {
+  const [filteredData, setFilteredData] = useState(data)
+  const [hiddenColumns, setHiddenColumns] = useState([])
+
+  const handleSearch = (query) => {
+    // TODO - logic for filtering the data based on search query
+  }
+
+  const handleToggleColumn = (column) => {
+    // TODO - logic to hide or show columns
+  }
 
   return (
     <>
-      <h1>HP Data Table</h1>
+      <SearchBar onSearch={handleSearch} />
+      <ColumnToggler
+        columns={[
+          'name',
+          'dateOfBirth',
+          'yearOfBirth',
+          'species',
+          'house',
+          'actor',
+          'image',
+          'patronus',
+        ]}
+        onToggle={handleToggleColumn}
+      />
+      <Table data={filteredData} hiddenColumns={hiddenColumns} />
     </>
   )
 }
---
+++ src/components/ColumnToggler.jsx
@@ -0,0 +1,5 @@
+const ColumnToggler = ({ columns, onToggle }) => {
+  // TODO - logic to toggle column visibility
+}
+
+export default ColumnToggler
